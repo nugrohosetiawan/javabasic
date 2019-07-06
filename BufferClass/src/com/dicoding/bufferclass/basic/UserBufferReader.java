b2c13f947192122e4477873e9f905ea7269675c3
nugrohosetiawan <nugpro@gmail.com>
2019-07-06 08:23:53 +0700

    add bufferclass

---
+++ BufferClass/src/com/dicoding/bufferclass/basic/UserBufferReader.java
@@ -0,0 +1,28 @@
+package com.dicoding.bufferclass.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UserBufferReader {
+    public static void main(String[] args) {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(isr);
+        System.out.println("Program Penjumlahan Sangat Sederhana ");
+        int value1 = 0;
+        int value2 = 0;
+
+        try {
+            System.out.print("Masukan Angka pertama : ");
+            value1 = Integer.parseInt(bufferedReader.readLine());//
+            System.out.print("Masukan Angka kedua : ");
+            value2 = Integer.parseInt(bufferedReader.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int hasil = value1 + value2;
+        System.out.println("Hasilnya adalah : " + hasil);
+
+    }
+}
