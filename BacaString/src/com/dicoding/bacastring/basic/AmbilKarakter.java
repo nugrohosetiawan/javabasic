806f316e71eafa83e3c5178589aae63b890aaecf
nugrohosetiawan <nugpro@gmail.com>
2019-07-06 08:54:29 +0700

    sting fundamental

---
+++ BacaString/src/com/dicoding/bacastring/basic/AmbilKarakter.java
@@ -0,0 +1,9 @@
+package com.dicoding.bacastring.basic;
+
+public class AmbilKarakter {
+    public static void main(String[] args) {
+        String dicoding = "dicoding";
+        char result = dicoding.charAt(6);
+        System.out.println(result);
+    }
+}
---
+++ BacaString/src/com/dicoding/bacastring/basic/BacaString.java
@@ -0,0 +1,11 @@
+package com.dicoding.bacastring.basic;
+
+public class BacaString {   // Panjang dari String
+    public static void main(String[] args) {
+        String dicoding = "dicoding";
+        int length = dicoding.length();
+        System.out.println("Panjang Karakter adalah : " + length);
+    }
+}
+
+
